cfd585e94590a8a960d5a706bc5a066716dd82b3
aadi350 <a.aadi380@gmail.com>
2021-04-10 08:13:32 -0400

    add soil params for potato and citrus

---
+++ app/predictyield/soilparams.py
@@ -0,0 +1,44 @@
+soil_params = dict({
+    'citrus': {
+        'nitrogen': {
+            'max': 95,
+            'min': 247
+        },
+        'potassium': {
+            'max': 102,
+            'min': 293
+        },
+        'phosporous': {
+            'max': 77,
+            'min': 116
+        },
+    },
+    'potato':  {
+        'nitrogen': {
+            'max': 118,
+            'min': 178
+        },
+        'potassium': {
+            'max': 118,
+            'min': 178
+        },
+        'phosporous': {
+            'max': 237,
+            'min': 355
+        }
+    },
+    'pigeonpeas': {
+        'nitrogen': {
+            'max': -1,
+            'min': -1
+        },
+        'potassium': {
+        'max': -1,
+        'min': -1
+        },
+        'phosporous': {
+            'max': -1,
+            'min': -1
+        }
+    },
+})
