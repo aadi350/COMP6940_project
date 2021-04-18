22b9b9d7d6cae666c429429f4bb93d5b0fe4f406
Kelan Laban <kelan.laban@gmail.com>
2021-04-18 05:15:49 -0400

    Changed credential file reference

--- app/fbaseconnection/firebaseconnection.py
+++ app/fbaseconnection/firebaseconnection.py
@@ -6,9 +6,10 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import credentials, firestore, initialize_app, db
 from flask import Flask, request, jsonify
+import os
 
-
-cred = credentials.Certificate('private/crop-jedi-storage-firebase-adminsdk-scef3-882ee18ae0.json')
+data = os.path.abspath(os.path.dirname(__file__)) + "\..\private\crop-jedi-storage-firebase-adminsdk-scef3-882ee18ae0.json"
+cred = credentials.Certificate(data)
 app = firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://crop-jedi-storage-default-rtdb.firebaseio.com/'
 })
